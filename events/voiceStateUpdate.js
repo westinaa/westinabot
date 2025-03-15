a5f943a5a61e104ef8008a0632b33deaaaef64bb
westinaa <162748407+westinaa@users.noreply.github.com>
2025-03-16 02:29:41 +0300

    Create events/voiceStateUpdate.js

---
+++ events/voiceStateUpdate.js
@@ -0,0 +1,21 @@
+const UserStats = require('../../models/userStats');
+
+module.exports = {
+  name: 'voiceStateUpdate',
+  async execute(oldState, newState) {
+    if (!newState.guild) return; // Eğer bir sunucuda değilse, devam etme
+
+    const userStats = await UserStats.findOne({ userId: newState.id });
+
+    if (newState.channel && !oldState.channel) {
+      // Kanalda aktif oldu
+      if (userStats) {
+        userStats.voiceActivity += 1; // Burada aktiflik süresini arttırabilirsin (saniye cinsinden)
+        await userStats.save();
+      }
+    } else if (!newState.channel && oldState.channel) {
+      // Kanaldan ayrıldı
+      // Aktiflik süresi eklenebilir veya başka bir işlem yapılabilir
+    }
+  },
+};
