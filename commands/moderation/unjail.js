8ef0b5a28dfdf5bb361fb44eb025bdddff51846d
westinaa <162748407+westinaa@users.noreply.github.com>
2025-03-15 12:27:26 +0300

    Update unjail.js

--- commands/moderation/unjail.js
+++ commands/moderation/unjail.js
@@ -1,6 +1,7 @@
 const { permissions } = require("../../utils/permissions.js");
 const logger = require("../../utils/logger.js");
 const { EmbedBuilder } = require("discord.js");
+const User = require("../../models/userModel.js"); // MongoDB modelini dahil et
 
 module.exports = {
     name: "unjail",
@@ -9,7 +10,7 @@ module.exports = {
         if (!permissions.checkModerator(message.member)) {
             const errorEmbed = new EmbedBuilder()
                 .setColor("#ff0000")
-                .setDescription("❌ Bu komutu kullanma yetkiniz yok!")
+                .setDescription("<a:westina_red:1349419144243576974> Bu komutu kullanma yetkiniz yok!")
                 .setFooter({ text: message.guild.name });
             return message.reply({ embeds: [errorEmbed] });
         }
@@ -18,22 +19,20 @@ module.exports = {
         if (!user) {
             const errorEmbed = new EmbedBuilder()
                 .setColor("#ff0000")
-                .setDescription(
-                    "❌ Hapisten çıkarılacak kullanıcıyı etiketlemelisiniz!",
-                )
+                .setDescription("<a:westina_red:1349419144243576974> Hapisten çıkarılacak kullanıcıyı etiketlemelisiniz!")
                 .setFooter({ text: message.guild.name });
             return message.reply({ embeds: [errorEmbed] });
         }
 
         const reason = args.slice(1).join(" ") || "Sebep belirtilmedi";
         const jailRole = message.guild.roles.cache.find(
-            (role) => role.name === "cezalı",
+            (role) => role.name === "cezalı"
         );
 
         if (!jailRole) {
             const errorEmbed = new EmbedBuilder()
                 .setColor("#ff0000")
-                .setDescription("❌ Jail rolü bulunamadı!")
+                .setDescription("<a:westina_red:1349419144243576974> Jail rolü bulunamadı!")
                 .setFooter({ text: message.guild.name });
             return message.reply({ embeds: [errorEmbed] });
         }
@@ -43,7 +42,7 @@ module.exports = {
             if (!user.roles.cache.has(jailRole.id)) {
                 const errorEmbed = new EmbedBuilder()
                     .setColor("#ff0000")
-                    .setDescription("❌ Bu kullanıcı hapiste değil!")
+                    .setDescription("<a:westina_red:1349419144243576974> Bu kullanıcı hapiste değil!")
                     .setFooter({ text: message.guild.name });
                 return message.reply({ embeds: [errorEmbed] });
             }
@@ -51,12 +50,17 @@ module.exports = {
             // Jail rolünü kaldır
             await user.roles.remove(jailRole);
 
+            // MongoDB veritabanında kullanıcıyı güncelle
+            await User.findOneAndUpdate(
+                { userID: user.id, guildID: message.guild.id },
+                { $set: { jailed: false } },
+                { new: true }
+            );
+
             const successEmbed = new EmbedBuilder()
                 .setColor("#98ff98")
-                .setTitle("🔓 Kullanıcı Hapisten Çıkarıldı")
-                .setDescription(
-                    `**${user.user.tag}** kullanıcısı hapisten çıkarıldı.`,
-                )
+                .setTitle("<a:westina_onay:1349184023867691088> Kullanıcı Hapisten Çıkarıldı")
+                .setDescription(`**${user.user.tag}** kullanıcısı hapisten çıkarıldı.`)
                 .addFields(
                     {
                         name: "👮 Moderatör",
@@ -80,9 +84,7 @@ module.exports = {
             console.error("Unjail hatası:", error);
             const errorEmbed = new EmbedBuilder()
                 .setColor("#ff0000")
-                .setDescription(
-                    "❌ Kullanıcı hapisten çıkarılırken bir hata oluştu!",
-                )
+                .setDescription("<a:westina_red:1349419144243576974> Kullanıcı hapisten çıkarılırken bir hata oluştu!")
                 .setFooter({ text: message.guild.name });
             message.reply({ embeds: [errorEmbed] });
         }
