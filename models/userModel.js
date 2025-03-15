14522a2ff4a2b28840957b95146ee6059b51bf40
westinaa <162748407+westinaa@users.noreply.github.com>
2025-03-15 16:06:09 +0300

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,13 +1,30 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
+// Kullanıcı modelini oluşturuyoruz
 const userSchema = new mongoose.Schema({
-    userId: { type: String, required: true },
-    guildId: { type: String, required: true },
-    jailEndTime: { type: Date, default: null }, // Süreli hapis için zaman
-    reason: { type: String, default: "Sebep belirtilmedi" },
-    moderatorId: { type: String, required: true },
-}, { timestamps: true });
+    userId: { type: String, required: true },  // Kullanıcı ID'si
+    guildId: { type: String, required: true }, // Sunucu ID'si
+    mutes: [
+        {
+            createdAt: { type: Date, default: Date.now },  // Mute tarihi
+            reason: { type: String, default: 'Sebep belirtilmemiş' },  // Mute nedeni
+        }
+    ],
+    jails: [
+        {
+            createdAt: { type: Date, default: Date.now },  // Jail uygulama tarihi
+            reason: { type: String, default: 'Sebep belirtilmemiş' },  // Jail nedeni
+        }
+    ],
+    bans: [
+        {
+            createdAt: { type: Date, default: Date.now },  // Ban tarihi
+            reason: { type: String, default: 'Sebep belirtilmemiş' },  // Ban nedeni
+        }
+    ]
+});
 
-const User = mongoose.model("User", userSchema);
+// Modeli oluştur
+const UserModel = mongoose.model('User', userSchema);
 
-module.exports = User;
+module.exports = UserModel;
