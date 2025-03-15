2d6e1095bb29c1dce8f3ca94281e2a1fcbbf0bad
westinaa <162748407+westinaa@users.noreply.github.com>
2025-03-15 14:41:27 +0300

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,33 +1,12 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    userId: {
-        type: String,
-        required: true,
-        unique: true,  // Her kullanıcı sadece bir kez kaydedilebilir
-    },
-    guildId: {
-        type: String,
-        required: true,
-    },
-    previousRoles: { type: [String], default: [] }, 
-    jailEndTime: {
-        type: Date,
-        required: true,
-    },
-    reason: {
-        type: String,
-        default: "Sebep belirtilmedi",
-    },
-    moderatorId: {
-        type: String,
-        required: true,
-    },
-    roles: {
-        type: [String],  // Kullanıcının geçici olarak alacağı roller
-        default: [],
-    },
-}, { timestamps: true });  // Kullanıcı cezalandırma işleminin zamanını kaydeder.
+    userId: { type: String, required: true },
+    guildId: { type: String, required: true },
+    jailEndTime: { type: Date, default: null }, // Süreli hapis için zaman
+    reason: { type: String, default: "Sebep belirtilmedi" },
+    moderatorId: { type: String, required: true },
+}, { timestamps: true });
 
 const User = mongoose.model("User", userSchema);
 
