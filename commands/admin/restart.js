8a9d5b29fdae9b04db26fa86dfd33bc0c6e381cd
westinaa <162748407+westinaa@users.noreply.github.com>
2025-03-15 15:57:17 +0300

    Update restart.js

--- commands/admin/restart.js
+++ commands/admin/restart.js
@@ -22,8 +22,7 @@ module.exports = {
                 name: message.client.user.username,  // Botun ismi
                 iconURL: message.client.user.displayAvatarURL()  // Botun profil resmi
             })
-            .setDescription("<a:aku_loading:1349419105622691911> Tüm komutları kaydedip sistemi tekrar başlatıyorum. \n<:seta:1346909730173354054> Bu işlem \`~ 1dk\` sürecektir.")
-            .setFooter({ text: `<t:${timestamp}:R>` })
+            .setDescription(`<t:${timestamp}:R> \n<a:aku_loading:1349419105622691911> Tüm komutları kaydedip sistemi tekrar başlatıyorum. \n<:seta:1346909730173354054> Bu işlem \`~ 1dk\` sürecektir.`)
             .setTimestamp();
 
         await message.channel.send({ embeds: [restartEmbed] });
