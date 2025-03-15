117a1e5e4cb735a65d8cbd9f60d86f99b072eb1b
westinaa <162748407+westinaa@users.noreply.github.com>
2025-03-15 17:21:31 +0300

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -8,18 +8,29 @@ const userSchema = new mongoose.Schema({
         {
             createdAt: { type: Date, default: Date.now },  // Mute tarihi
             reason: { type: String, default: 'Sebep belirtilmemiş' },  // Mute nedeni
+            moderatorId: { type: String, required: true },  // Cezayı veren moderatörün ID'si
         }
     ],
     jails: [
         {
             createdAt: { type: Date, default: Date.now },  // Jail uygulama tarihi
             reason: { type: String, default: 'Sebep belirtilmemiş' },  // Jail nedeni
+            moderatorId: { type: String, required: true },  // Cezayı veren moderatörün ID'si
         }
     ],
     bans: [
         {
             createdAt: { type: Date, default: Date.now },  // Ban tarihi
             reason: { type: String, default: 'Sebep belirtilmemiş' },  // Ban nedeni
+            moderatorId: { type: String, required: true },  // Cezayı veren moderatörün ID'si
+        }
+    ],
+    vmutes: [ // Yeni alan: VMute logları
+        {
+            createdAt: { type: Date, default: Date.now },  // VMute tarihi
+            reason: { type: String, default: 'Sebep belirtilmemiş' },  // VMute nedeni
+            endedAt: { type: Date, required: true },  // VMute bitiş tarihi
+            moderatorId: { type: String, required: true },  // Cezayı veren moderatörün ID'si
         }
     ]
 });
