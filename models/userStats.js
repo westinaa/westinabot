e39059b218d6a047368475a3a39a41ab986f753c
westinaa <162748407+westinaa@users.noreply.github.com>
2025-03-16 03:05:20 +0300

    Update userStats.js

--- models/userStats.js
+++ models/userStats.js
@@ -1,15 +1,10 @@
+// models/userStats.js
 const mongoose = require('mongoose');
 
 const userStatsSchema = new mongoose.Schema({
   userId: { type: String, required: true },
-  messages: { type: Map, of: Number, default: {} },  // Mesaj sayısı
-  voiceStats: [{
-    channelId: String,
-    joinTime: Date,
-    leaveTime: Date,
-    totalTime: { type: Number, default: 0 }  // Sesli kanalda geçirilen toplam süre
-  }],
-  lastUpdated: { type: Date, default: Date.now }
+  messages: { type: Number, default: 0 },
+  voiceActivity: { type: Number, default: 0 },
 });
 
 const UserStats = mongoose.model('UserStats', userStatsSchema);
