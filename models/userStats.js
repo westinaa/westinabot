88f9fcff1a390fd34c9172f20e7c51846cfce902
westinaa <162748407+westinaa@users.noreply.github.com>
2025-03-16 03:35:41 +0300

    Update userStats.js

--- models/userStats.js
+++ models/userStats.js
@@ -1,12 +1,9 @@
-// models/userStats.js
 const mongoose = require('mongoose');
 
 const userStatsSchema = new mongoose.Schema({
   userId: { type: String, required: true },
   messages: { type: Number, default: 0 },
-  voiceActivity: { type: Number, default: 0 },
+  voiceActivity: { type: Number, default: 0 }
 });
 
-const UserStats = mongoose.model('UserStats', userStatsSchema);
-
-module.exports = UserStats;
+module.exports = mongoose.model('UserStats', userStatsSchema);
