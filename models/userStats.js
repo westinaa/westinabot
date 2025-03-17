a2faff832e5ec0baf33972b10ed3d303a2f0d1c4
westinaa <162748407+westinaa@users.noreply.github.com>
2025-03-17 13:27:22 +0300

    Update userStats.js

--- models/userStats.js
+++ models/userStats.js
@@ -3,7 +3,15 @@ const mongoose = require('mongoose');
 const userStatsSchema = new mongoose.Schema({
   userId: { type: String, required: true },
   messages: { type: Number, default: 0 },
-  voiceActivity: { type: Number, default: 0 }
+  voiceHours: { type: Number, default: 0 }, // Saat olarak kayıt
+  voiceMinutes: { type: Number, default: 0 }, // Dakika olarak kayıt
+  invites: {
+    total: { type: Number, default: 0 }, // Toplam davet
+    real: { type: Number, default: 0 },  // Gerçek davet
+    bonus: { type: Number, default: 0 }, // Bonus davet
+    left: { type: Number, default: 0 },  // Ayrılan davet
+    fake: { type: Number, default: 0 }   // Fake davet
+  }
 });
 
 module.exports = mongoose.model('UserStats', userStatsSchema);
