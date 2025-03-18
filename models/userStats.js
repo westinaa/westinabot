1fe42a922961f6bf340dcc8db20bfc87cedfe36e
westinaa <w3stiin4@gmail.com>
2025-03-19 02:31:51 +0300

    Update userStats.js

--- models/userStats.js
+++ models/userStats.js
@@ -3,15 +3,22 @@ const mongoose = require('mongoose');
 const userStatsSchema = new mongoose.Schema({
   userId: { type: String, required: true },
   messages: { type: Number, default: 0 },
-  voiceHours: { type: Number, default: 0 }, // Saat olarak kayıt
-  voiceMinutes: { type: Number, default: 0 }, // Dakika olarak kayıt
+  dailyMessages: { type: Number, default: 0 },
+  weeklyMessages: { type: Number, default: 0 },
+  voiceHours: { type: Number, default: 0 },
+  voiceMinutes: { type: Number, default: 0 },
+  voiceWeeklyHours: { type: Number, default: 0 },
+  voiceWeeklyMinutes: { type: Number, default: 0 },
+  voiceDailyHours: { type: Number, default: 0 },
+  voiceDailyMinutes: { type: Number, default: 0 },
   invites: {
-    total: { type: Number, default: 0 }, // Toplam davet
-    real: { type: Number, default: 0 },  // Gerçek davet
-    bonus: { type: Number, default: 0 }, // Bonus davet
-    left: { type: Number, default: 0 },  // Ayrılan davet
-    fake: { type: Number, default: 0 }   // Fake davet
-  }
+    total: { type: Number, default: 0 },
+    real: { type: Number, default: 0 },
+    bonus: { type: Number, default: 0 },
+    left: { type: Number, default: 0 },
+    fake: { type: Number, default: 0 }
+  },
+  lastWeeklyReset: { type: Date, default: Date.now } // Haftalık sıfırlama tarihi
 });
 
 module.exports = mongoose.model('UserStats', userStatsSchema);
