a4d39eb4697646e09d5b2e6d229752d8840768e9
westinaa <w3stiin4@gmail.com>
2025-03-19 02:46:48 +0300

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,5 +1,6 @@
 const { Events } = require('discord.js'); 
 const UserStats = require('../models/userStats.js');
+const moment = require('moment');
 
 module.exports = {
   name: Events.voiceStateUpdate,
@@ -33,6 +34,28 @@ module.exports = {
           userStats.voiceMinutes = (userStats.voiceMinutes || 0) + minutes;
           userStats.voiceHours = Math.floor(userStats.voiceMinutes / 60);
           userStats.voiceMinutes = userStats.voiceMinutes % 60;
+
+          // Haftalık ses verisini güncelle
+          userStats.voiceWeeklyMinutes = (userStats.voiceWeeklyMinutes || 0) + minutes;
+          userStats.voiceWeeklyHours = Math.floor(userStats.voiceWeeklyMinutes / 60);
+          userStats.voiceWeeklyMinutes = userStats.voiceWeeklyMinutes % 60;
+
+          // Günlük ses verisini güncelle
+          userStats.voiceDailyMinutes = (userStats.voiceDailyMinutes || 0) + minutes;
+          userStats.voiceDailyHours = Math.floor(userStats.voiceDailyMinutes / 60);
+          userStats.voiceDailyMinutes = userStats.voiceDailyMinutes % 60;
+
+          // Haftalık sıfırlama kontrolü
+          const now = moment();
+          const lastWeeklyResetMessages = moment(userStats.lastWeeklyResetMessages);
+          const lastWeeklyResetVoice = moment(userStats.lastWeeklyResetVoice);
+
+          // Pazartesi günü haftalık veriyi sıfırlıyoruz
+          if (now.isoWeekday() === 1 && now.isoWeek() !== lastWeeklyResetMessages.isoWeek()) {
+            userStats.voiceWeeklyMinutes = 0;
+            userStats.voiceWeeklyHours = 0;
+            userStats.lastWeeklyResetVoice = now.toDate();  // Haftalık ses sıfırlama zamanı
+          }
 
           // Veritabanında güncelleniyor
           await userStats.save();
